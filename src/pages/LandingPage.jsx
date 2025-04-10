3ec49b35c4b5531c48ceb47abc29581e31717a57
DaviVCarvalhos <151264268+DaviVCarvalhos@users.noreply.github.com>
2025-04-10 12:17:27 -0300

    feat: adição de landing page

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,19 @@
+import Header from "../components/Header";
+import Hero from "../components/Hero";
+import Features from "../components/Features";
+import Testimonials from "../components/Testimonials";
+import CTA from "../components/CTA";
+import Footer from "../components/Footer";
+
+export default function LandingPage() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Features />
+      <Testimonials />
+      <CTA />
+      <Footer />
+    </>
+  );
+}
